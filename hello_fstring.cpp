81495e0dbed3723c79477b5c504568b0ad40e962
shai <shai@waves.com>
2022-12-09 15:32:02 +0200

    zcode project and hello_fstring.cp

---
+++ hello_fstring.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+#include "fstring.h"
+
+int main(int argc, char** argv)
+{
+    std::cout << "sizeof(fixed::fstring_base<0, char>) = " << sizeof(fixed::fstring_base<0, char>)  << std::endl;
+    std::cout << "sizeof(fixed::fstring15) = " << sizeof(fixed::fstring15)  << std::endl;
+    std::cout << "sizeof(fixed::fstring31) = " << sizeof(fixed::fstring31)  << std::endl;
+    std::cout << "sizeof(fixed::fstring63) = " << sizeof(fixed::fstring63)  << std::endl;
+    std::cout << "sizeof(fixed::fstring_ref) = " << sizeof(fixed::fstring_ref)  << std::endl;
+
+    fixed::fstring31 fstr("Hello fstring");
+    std::cout << fstr << std::endl;
+
+
+
+    return 0;
+}
